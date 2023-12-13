2c17ab0291a2e1f7da4e42f564db7a2d9961cda9
Ben <manglebot@users.noreply.github.com>
2023-12-13 12:08:11 +0000

    fix:make product page work

--- src/components/products/product/product.tsx
+++ src/components/products/product/product.tsx
@@ -7,7 +7,7 @@ const Product = () => {
     <section className="content has-text-centered">
       <p>Welcome to the page for product:</p>
       <p>
-        <strong className="is-size-1"> {productId.product} </strong>
+        <strong className="is-size-1"> {productId.productId} </strong>
       </p>
     </section>
   );
