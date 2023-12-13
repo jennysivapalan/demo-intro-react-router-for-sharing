172a9139f52740948aece9cc79d97609d4ca07d0
Ben <manglebot@users.noreply.github.com>
2023-12-13 11:32:05 +0000

    fix:make it work

--- src/components/layouts/main_layouts.tsx
+++ src/components/layouts/main_layouts.tsx
@@ -1,1 +1,18 @@
-export {}
+import { Outlet } from "react-router-dom";
+import Footer from "../footer/footer";
+import Header from "../header/header";
+import Home from "../home/home";
+
+function MainLayout() {
+  return (
+    <>
+      <Header />
+      <main className="p-6" style={{ flex: "1" }}>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default MainLayout;
--- src/components/router/router.tsx
+++ src/components/router/router.tsx
@@ -1,2 +1,21 @@
-export {}
-// what goes here??
+import { Routes, Route } from "react-router-dom";
+import MainLayout from "../layouts/main_layouts";
+import Home from "../home/home";
+import Products from "../products/products";
+import About from "../about/about";
+import Help from "../help/help";
+import NotFound from "../not_found/not_found";
+
+const Router: React.FC = () => (
+  <Routes>
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<Home />} />
+      <Route path="products" element={<Products />} />
+      <Route path="about" element={<About />} />
+      <Route path="help" element={<Help />} />
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  </Routes>
+);
+
+export default Router;
